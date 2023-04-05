0795bfba47635fb9ab5a49bb605a171e288f653f
NataliiaDanchenko <danchnataly@gmail.com>
2023-04-05 18:41:59 +0300

    commit_2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 
 import './App.css';
+import Header from "./Components/Header/Header";
+import ProductCategories from "./Components/ProductCategories/ProductCategories";
+import Cards from "./Components/Cards/Cards";
+import Product from "./Components/Product/Product";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Header/>
+  },
+  {
+    path: "about",
+    element: <ProductCategories/>
+  },
+  {
+    path: "shop",
+    element: <Cards/>
+  },
+  {
+    path: "product/:productId",
+    element: <Product/>
+  }
+])
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+  return (
+      <div>
+        <RouterProvider router={router}/>
+      </div>
+  )
 }
 
 export default App;
+
